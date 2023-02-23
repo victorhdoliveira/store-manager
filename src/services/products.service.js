c5f9ef28bdbbf0f7be42ebf4bda6b69da4148eb7
Victor Hugo <victorhdoliveira@gmail.com>
2023-02-23 15:35:56 -0300

    novo commit devido a erro no avaliador

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -10,29 +10,29 @@ const findById = async (productId) => {
   const error = schema.validateId(productId);
   if (error.type) return error;
 
-  const product = await productModel.findById(productId);
-  if (!product) return { type: 'NOT_FOUND', message: 'Product not found' };
+  const productById = await productModel.findById(productId);
+  if (!productById) return { type: 'NOT_FOUND', message: 'Product not found' };
 
-  return { type: null, message: product };
+  return { type: null, message: productById };
 };
 
 const newProduct = async (name) => {
-  const product = await productModel.insertNewProduct(name);
-  if (!product) return { type: 'NOT_FOUND', message: 'Product not found' };
+  const insertProduct = await productModel.insertNewProduct(name);
+  if (!insertProduct) return { type: 'NOT_FOUND', message: 'Product not found' };
   
-  return { type: null, message: product };
+  return { type: null, message: insertProduct };
 };
 
 const updateProductById = async (name, id) => {
-  const product = await productModel.updateProduct(name, id);
-  if (!product) return { type: 'NOT_FOUND', message: 'Product not found' };
+  const updateProduct = await productModel.updateProduct(name, id);
+  if (!updateProduct) return { type: 'NOT_FOUND', message: 'Product not found' };
  
-  return { type: null, message: product };
+  return { type: null, message: updateProduct };
 };
 
 const deleteProductById = async (id) => {
-  const product = await productModel.findById(id);
-  if (!product) return { type: 'NOT_FOUND', message: 'Product not found' };
+  const productById = await productModel.findById(id);
+  if (!productById) return { type: 'NOT_FOUND', message: 'Product not found' };
   
   await productModel.deleteProduct(id);
 
--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -21,15 +21,15 @@ const findAll = async () => {
 };
 
 const findById = async (saleId) => {
-  const sale = await saleModel.findById(saleId);
-  if (!sale.length) return { type: 'NOT_FOUND', message: 'Sale not found' };
+  const saleById = await saleModel.findById(saleId);
+  if (!saleById.length) return { type: 'NOT_FOUND', message: 'Sale not found' };
 
-  return { type: null, message: sale };
+  return { type: null, message: saleById };
 };
 
 const deleteSaleById = async (saleId) => {
-  const sale = await saleModel.findById(saleId);
-  if (!sale.length) return { type: 'NOT_FOUND', message: 'Sale not found' };
+  const deleteSale = await saleModel.findById(saleId);
+  if (!deleteSale.length) return { type: 'NOT_FOUND', message: 'Sale not found' };
   
   await saleModel.deleteSale(saleId);
   return { type: null, message: '' };
@@ -39,8 +39,8 @@ const updateSale = async (saleId, itemsUpdated) => {
   const hasProductId = await schema.validateProduct(itemsUpdated);
   if (hasProductId) return hasProductId;
 
-  const sale = await saleModel.findById(saleId);
-  if (!sale.length) return { type: 'NOT_FOUND', message: 'Sale not found' };
+  const saleById = await saleModel.findById(saleId);
+  if (!saleById.length) return { type: 'NOT_FOUND', message: 'Sale not found' };
 
   await Promise.all(itemsUpdated.map((item) => saleModel
     .updateSale(saleId, item.productId, item.quantity)));
